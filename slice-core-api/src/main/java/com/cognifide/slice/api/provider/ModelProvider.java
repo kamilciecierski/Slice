3efa8cdfc8156169850633d3ac88e363ffc83063
kamilciecierski <kamilciecierski@cognifide.com>
2013-03-04 14:06:45 +0100

    SLICE-22 ModelProvider.java documentation fix. Added information about null returning by ModelProvider methods

--- slice-core-api/src/main/java/com/cognifide/slice/api/provider/ModelProvider.java
+++ slice-core-api/src/main/java/com/cognifide/slice/api/provider/ModelProvider.java
@@ -1,6 +1,6 @@
 package com.cognifide.slice.api.provider;
 
-/*
+/*-
  * #%L
  * Slice - Core API
  * $Id:$
@@ -22,7 +22,6 @@ package com.cognifide.slice.api.provider;
  * #L%
  */
 
-
 import java.util.Iterator;
 import java.util.List;
 
@@ -41,8 +40,8 @@ public interface ModelProvider {
 	 * {@link ExecutionContextStack} is modified - path attribute is added on top of execution stack. It
 	 * allows for recursive call of ModelProvider methods from model object that is being created.
 	 * 
-	 * It is possible to use absolute and relative (with "./" prefix) paths in recursive calls.
-	 * All gets are performed with Context that was used to create this ModelProvider.
+	 * It is possible to use absolute and relative (with "./" prefix) paths in recursive calls. All gets are
+	 * performed with Context that was used to create this ModelProvider.
 	 * 
 	 * <code>
 	 *   modelProvider.get(ModelType.class, "/absolute/patch");
@@ -54,7 +53,9 @@ public interface ModelProvider {
 	 * @parem T type of model object to create
 	 * @param type class of model object to create
 	 * @param path CRX repository path to create object from
-	 * @return model object from given CRX repository path
+	 * @return model object from given CRX repository path. It may return <code>null</code> depending on
+	 * provider implementation for specified class.
+	 * @see com.google.inject.Injector#getInstance(com.google.inject.Key) Injector.getInstance.
 	 */
 	<T> T get(final Class<T> type, final String path);
 
@@ -68,7 +69,9 @@ public interface ModelProvider {
 	 * @parem T type of model object to create
 	 * @param type class of model object to create
 	 * @param resource Sling resource to create object from
-	 * @return model object from given resource
+	 * @return model object from given resource. It may return <code>null</code> depending on provider
+	 * implementation for specified class.
+	 * @see com.google.inject.Injector#getInstance(com.google.inject.Key) Injector.getInstance.
 	 */
 	<T> T get(final Class<T> type, final Resource resource);
 
@@ -79,24 +82,26 @@ public interface ModelProvider {
 	 * 
 	 * @param className canonical name of a class to be resolved, e.g. com.cognifide.slice.api.ModelProvider
 	 * @param path CRX repository path to create object from
-	 * @return model object of specified className from given CRX repository path
-	 * @throws ClassNotFoundException if specified className cannot be resolved by Injector,
+	 * @return model object of specified className from given CRX repository path. It may return
+	 * <code>null</code> depending on provider implementation for specified class.
+	 * @see com.google.inject.Injector#getInstance(com.google.inject.Key) Injector.getInstance.
+	 * @throws ClassNotFoundException if specified className cannot be resolved by Injector.
 	 * {@link ClassNotFoundException} is thrown
 	 */
 	Object get(final String className, final String path) throws ClassNotFoundException;
 
 	/**
 	 * Creates list of model objects of type T from given CRX repository paths. During this method call state
-	 * of {@link ExecutionContextStack} is modified for each path from iterator - path is added on top of
-	 * path stack. It allows for recursive call of ModelProvider methods from model objects that are being
-	 * created.
+	 * of {@link ExecutionContextStack} is modified for each path from iterator - path is added on top of path
+	 * stack. It allows for recursive call of ModelProvider methods from model objects that are being created.
 	 * 
 	 * It is possible to use absolute and relative (with "./" prefix) paths in recursive calls.
 	 * 
 	 * @param T type of model objects to create
 	 * @param type class of model objects to create
 	 * @param paths iterator that returns CRX repository paths to create objects from
-	 * @return list of model objects from given CRX repository paths
+	 * @return list of model objects from given CRX repository paths. Returns empty list if <i>paths</i>
+	 * argument is <code>null</code>.
 	 */
 	<T> List<T> getList(final Class<T> type, final Iterator<String> paths);
 
@@ -106,7 +111,8 @@ public interface ModelProvider {
 	 * @param T type of model objects to create
 	 * @param type class of model objects to create
 	 * @param paths array of CRX repository paths to create objects from
-	 * @return list of model objects from given CRX repository paths
+	 * @return list of model objects from given CRX repository paths. Returns empty list if <i>paths</i>
+	 * argument is <code>null</code>.
 	 */
 	<T> List<T> getList(final Class<T> type, final String[] paths);
 }
